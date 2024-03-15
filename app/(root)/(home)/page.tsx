4379024bbbe43a9fdd091b5a470b3c932d559b3e
Daniela Romanov <danielar0man0v000@gmail.com>
2024-03-15 12:05:27 -0700

    commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+     
     </div>
   );
 }
