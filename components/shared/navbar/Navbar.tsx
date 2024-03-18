e273812d5f296bd245b9c17e74767f15b50b6c17
Daniela Romanov <danielar0man0v000@gmail.com>
2024-03-18 15:01:47 +0200

    navbarMobile

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import React from 'react'
 import { SignedIn, UserButton} from '@clerk/clerk-react'
 import Theme from './Theme'
+import MobileNav from './MobileNav'
 
 
 
@@ -13,7 +14,7 @@ const Navbar = () => {
     <nav className='flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>
       <Link href="/" className='flex items-center gap-1'>
         <Image 
-           src="/public/assets/images/site-logo.svg" alt='DevFlow' width={23} height={23}
+           src="/assets/images/site-logo.svg" alt='DevFlow' width={23} height={23}
            />
            <p className='h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden'>Dev <span className='text-primary-500'>Overflow</span></p>
       </Link>
@@ -32,7 +33,7 @@ const Navbar = () => {
             }}
             />
             </SignedIn>
-            Mobile Navbar
+            <MobileNav />
         </div>
     </nav>
   )
--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -20,12 +20,12 @@ const Theme = () => {
   <MenubarMenu>
     <MenubarTrigger className='focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200'>
         {mode === "light" ? (
-             <Image src="/public/assets/icons/sun.svg" 
+             <Image src="/assets/icons/sun.svg" 
              alt="sun" 
              width={20} 
              height={20} 
              className="active-theme"/>): (
-             <Image src="/public/assets/icons/moon.svg" 
+             <Image src="/assets/icons/moon.svg" 
              alt="moon" 
              width={20} 
              height={20} 
